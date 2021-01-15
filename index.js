7c7633848c33fc164c49213743fd54b93084b6d7
Egor Kochkin <egurt96@yandex.ru>
2021-01-15 13:27:27 +0300

    small file-naming  and save fixes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const axios = require("axios");
 const Path = require("path");
-let globalName = 1;
 
 function readLinkFromTxt() {
   try {
@@ -16,8 +15,11 @@ function readLinkFromTxt() {
 function getUrlFromPost(url) {
   let jsonPost = url + "?__a=1";
   return axios.get(jsonPost).then((response) => {
-    let urlArray = [];
+    let urlArray = []
     let type = response.data["graphql"]["shortcode_media"]["__typename"];
+    let userName = response.data["graphql"]["shortcode_media"]["owner"]["username"]
+    let rawData = new Date(Number(response.data["graphql"]["shortcode_media"]["taken_at_timestamp"]) * 1000)
+    let photoDate = `${rawData.getFullYear()}-${String(rawData.getMonth() + 1).padStart(2,'0')}-${String(rawData.getDate()).padStart(2,'0')}`
     if (type == "GraphSidecar") {
       let nodeArray =
         response.data["graphql"]["shortcode_media"]["edge_sidecar_to_children"][
@@ -25,20 +27,26 @@ function getUrlFromPost(url) {
         ];
       for (node of nodeArray) {
         let url = node["node"]["display_url"];
-        urlArray.push(url);
+        let id = node["node"]["id"];
+        urlArray.push([url,photoDate,id,userName]);
       }
     }
     if (type == "GraphImage") {
       let url = response.data["graphql"]["shortcode_media"]["display_url"];
-      urlArray.push(url);
+      let id = response.data["graphql"]["shortcode_media"]["id"];
+      urlArray.push([url,photoDate,id,username]);
     }
     return urlArray;
   });
 }
 
-async function downloadImage(url) {
-  const path = Path.resolve(__dirname, "images", `${globalName}.png`);
-  globalName++;
+async function downloadImage(array) {
+  let [url,photoDate,id,userName] = array
+  let dir = `./images/${userName}`
+  if(!fs.existsSync(dir)){
+    fs.mkdirSync(dir)
+  }
+  const path = Path.resolve(__dirname, dir, `${photoDate} ${id}.jpg`);
   const writer = fs.createWriteStream(path);
   const response = await axios({
     url,
@@ -58,7 +66,7 @@ function start() {
   let links = readLinkFromTxt();
   for (link of links) {
     getUrlFromPost(link).then((urlsArray) => {
-      urlsArray.map((url) => downloadImage(url));
+      urlsArray.map((array) => downloadImage(array));
     });
   }
   console.log("done");
